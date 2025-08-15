134a4f9ea7b765020a4aa8207c6977271d6bb482
hisao_X-server <hisao52321983@gmail.com>
2025-08-16 07:03:42 +0900

    feat limit_task

--- todoproject/todo/forms.py
+++ todoproject/todo/forms.py
@@ -4,4 +4,7 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'completed']  # モデルに応じて調整
+        fields = ['title', 'description', 'completed', 'due_date']  # モデルに応じて調整
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'})  # カレンダー入力に
+        }
